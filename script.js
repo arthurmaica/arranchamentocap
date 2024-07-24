f7db10ce0322ca06a31ce8c2e47f006e3a411f45
arthurmaica <arthurmaicaa@gmail.com>
2024-07-24 17:23:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function salvarSolicitacao() {
-    // Obtém os valores dos campos do formulário
     var posto = document.getElementById("posto").value;
     var nome = document.getElementById("nome").value;
     var solicitacao = document.getElementById("solicitacao").value;
 
-    // Constrói o objeto com os dados a serem enviados
     var data = {
         posto: posto,
         nome: nome,
         solicitacao: solicitacao
     };
 
-    // Faz uma requisição POST para a sua API no servidor
-    fetch('https://seu_servidor.com/api/salvar_solicitacao.php', {
+    // Endpoint para sua API Node.js que atualiza o arquivo no GitHub
+    var apiUrl = 'http://localhost:3000/api/salvar-solicitacao'; // Substitua pela URL de sua API
+
+    fetch(apiUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -26,8 +26,8 @@ function salvarSolicitacao() {
         return response.json();
     })
     .then(data => {
-        alert(data.message); // Exibe uma mensagem de sucesso
-        document.getElementById("requestForm").reset(); // Limpa o formulário após o envio
+        alert(data.message);
+        document.getElementById("requestForm").reset();
     })
     .catch(error => {
         console.error('Erro:', error);
